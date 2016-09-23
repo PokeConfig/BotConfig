d0792dfc50f4b0db8ab1f20825c823897716e0c2
kevinlife <kevinlife@gmail.com>
2016-09-23 15:52:17 +0800

    Create userdata.js

---
+++ userdata.js
@@ -0,0 +1,12 @@
+var userInfo = {
+   users: [{
+       enable: true,
+       username: "",
+       socketAddress: "0.0.0.0:4000"
+   }],
+   userZoom: false,
+   userPath: true,
+   zoom: 16,
+   userFollow: true,
+   gMapsAPIKey: ""
+};
